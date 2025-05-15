bfba03c4e728d11b2b7abae274446ca873f1f808
claytonsweather <claysweather@gmail.com>
2025-05-15 10:23:38 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+EMAIL_SENDER = "claysweather@gmail.com"
+EMAIL_PASSWORD = "2020Rewind"
+EMAIL_RECIPIENTS = ["ag1brgjbkvurb3hywpaylojfhcjb5lem@gw.notify.events"]
+
+LOCATIONS = [
+    "Houston", "Angleton", "West Columbia", "Huntsville",
+    "Crystal City", "Rocksprings", "New Braunfels"
+]
