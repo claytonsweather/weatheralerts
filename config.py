021d3ebb20ba4036698d02d433373d736f90a0e5
claytonsweather <claysweather@gmail.com>
2025-05-15 10:29:38 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-EMAIL_SENDER = "claysweather@gmail.com"
-EMAIL_PASSWORD = "2020Rewind"
-EMAIL_RECIPIENTS = ["ag1brgjbkvurb3hywpaylojfhcjb5lem@gw.notify.events"]
+import os
+
+EMAIL_SENDER = os.environ["EMAIL_SENDER"]
+EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
+EMAIL_RECIPIENTS = [os.environ["EMAIL_RECIPIENT"]]
 
 LOCATIONS = [
     "Houston", "Angleton", "West Columbia", "Huntsville",
